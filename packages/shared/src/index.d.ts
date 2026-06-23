67eb43c1c36128b835ea8d3dc8f0f193d38e0839
Ian Zhang <1036157809@qq.com>
2026-06-23 13:41:09 +0800

    perf: font optimize

--- packages/shared/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export type { ApiResponse, ChatMessage, FileMetadata, AircraftState, StatesResponse, TrackPoint, TracksResponse, VisitorStats } from './types/api';
-export type { Contact, Feedback, UploadedFile, RequestContext } from './types/models';
-export { validators } from './utils/validators';
-export { ROUTES, THEMES, LANGUAGES } from './constants';
-//# sourceMappingURL=index.d.ts.map
--- packages/shared/src/types/api.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * 通用 API 响应类型
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=api.js.map
